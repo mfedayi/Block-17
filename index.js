0a5b50a9e930f71d4b18591ca1a90787d26cb193
Mustafa Fedayi <mustafafedayi@macbookpro.myfiosgateway.com>
2025-03-26 17:31:24 -0400

    Assignent Block17

--- index.js
+++ index.js
@@ -11,27 +11,52 @@ class Numbers {
   }
   count() {
     //return the count of numbers in data
+    return this.data.length;
   }
-  printNumbers() {
+  printNumbers(data) {
     //print the numbers in data
+    this.data.forEach((element) => {
+      console.log(element);
+    });
   }
   odds() {
     //return the odd numbers in data
+    return this.data.filter((e) => e % 2 !== 0);
   }
   evens() {
     //return the even numbers in data
+    return this.data.filter((e) => e % 2 === 0);
   }
   sum() {
     //return the sum of the numbers
+   return this.data.reduce((total, num) => total + num)
+
+    // let sums = 0;
+    // for (let e of this.data) {
+    //   sums += e;
+    // }
+    // console.log(`The Sum: ${sums}`);
+    // return sums;
   }
   product() {
     //return the product of the numbers
+    return this.data.reduce((total, num) => total * num);
   }
   greaterThan(target) {
+    return this.data.filter((e) => e > target);
     //return the numbers greater than the target
+    // const nums = [];
+    // for(let num of this.data){
+    //   if(num > target) {
+    //   nums.push(num)
+    //   }
+
+    // }
+    // return nums;
   }
   howMany(target) {
     //return the count of a given number
+    return this.data.filter((e) => e === target).length;
   }
 }
 
